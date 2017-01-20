3f80fe8310743c7095029379c716d130d0b5043f
stevenPlas <steven.h.plas@gmail.com>
2017-01-20 15:41:14 -0500

    adding plants file, and updates to them.

---
+++ Chapter1/plants/plants.js
@@ -0,0 +1,15 @@
+/*JavaScript 6th Edition
+Chapter 1
+Chapter case
+
+Tinley Xeriscapes
+Author: Steven Plas
+Date: 1-20-17
+
+Filename: plants.js
+*/
+
+//define  fariables containing img src values
+var blanket = "images/blanket.jpg";
+var bluestem = "images/bluestem.jpg";
+var rugosa = "images/rugosa.jpg";
