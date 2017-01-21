ae3b40a225866d50932f3761d2d54e1e33db9d54
stevenPlas <steven.h.plas@gmail.com>
2017-01-21 11:21:12 -0500

    Adding event listeners to ft.js

--- Chapter2/Chapter/ft.js
+++ Chapter2/Chapter/ft.js
@@ -16,6 +16,16 @@ var totalCost = 0;
 var memoryBook = false;
 var reproductionRights = false;
 
+function calcStaff() {
+  var num = document.getElementById("photognum");
+  var hrs = document .getElementById("photoghrs");
+
+  totalCost -= photographerCost;
+  photographerCost = num.value * 100 * hrs.value;
+  totalCost += photographerCost;
+  document.getElementById("estimate").innerHTML = "$" + totalCost;
+}
+
 // sets all form field values to defaults
 function resetForm() {
   document.getElementById("photognum").value = 1;
@@ -23,6 +33,14 @@ function resetForm() {
   document.getElementById("membook").checked = memoryBook;
   document.getElementById("reprodrights").checked = reproductionRights;
   document.getElementById("distance").value = 0;
+  calcStaff();
+  createEventListeners();
+}
+
+//creates event listeners
+function createEventListeners() {
+  document.getElementById("photognum").addEventListener("change", calcStaff, false);
+  document.getElementById("photoghrs").addEventListener("change", calcStaff, false);
 }
 
 // resets for when page is reloaded
