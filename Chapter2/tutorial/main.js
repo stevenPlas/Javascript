8b85b1d3c5ae49b7ae23fec7d818da1a82424eff
stevenPlas <steven.h.plas@gmail.com>
2017-02-10 08:57:51 -0500

    Updates to splash page and tutorial for chapter 2

---
+++ Chapter2/tutorial/main.js
@@ -0,0 +1,24 @@
+var newItemCounter = 1;
+var ourList = document.querySelector("#our-list");
+var ourButton = document.getElementById("our-button");
+var ourHeadline = document.getElementById("our-headline");
+var listItems = document.querySelectorAll("#our-list li");
+
+ourList.addEventListener("click", activateItem, false);
+
+function activateItem(e) {
+  if (e.target.nodeName == "LI") {
+    ourHeadline.innerHTML = e.target.innerHTML;
+    for(i = 0; i < e.target.parentNode.children.length; i++){
+      e.target.parentNode.children[i].classList.remove("active");
+    }
+    e.target.classList.add("active");
+  }
+}
+
+ourButton.addEventListener("click", createNewItem, false);
+
+function createNewItem() {
+  ourList.innerHTML += "<li>Something new " + newItemCounter + "</li>"
+  newItemCounter++
+}
