dc411af494621698c097ff14a98602cb89c9756f
stevenPlas <steven.h.plas@gmail.com>
2017-03-10 15:11:58 -0500

    Adding updates for chap6, and splash.js

---
+++ SplashPage/splash.js
@@ -0,0 +1,44 @@
+/*
+*    Author:     Steven plas
+*    Date:       3/10/17
+*    filename:   splash.js
+*    Use:        js file for splash page
+*/
+
+//Chapter 1
+var plants = document.getElementById("plants").href = "http://wcccdesign.com/plas/chapter1/plants/plants.html";
+var proj11 = document.getElementById("1-1").href = "http://wcccdesign.com/plas/chapter1/HandsOnProject1-1/index.html";
+var proj13 = document.getElementById("1-3").href = "http://wcccdesign.com/plas/chapter1/HandsOnProject1-3/index.html";
+var proj14 = document.getElementById("1-4").href = "http://wcccdesign.com/plas/chapter1/HandsOnProject1-4/index.html";
+
+//Chapter 2
+var digital = document.getElementById("digital").href = "http://wcccdesign.com/plas/chapter2/chapter/digital.html";
+var estimate = document.getElementById("estimate").href = "http://wcccdesign.com/plas/chapter2/Chapter/estimate.html";
+var proj21 = document.getElementById("2-1").href = "http://wcccdesign.com/plas/chapter2/HandsOnProject2-1/index.html";
+var proj23 = document.getElementById("2-3").href = "http://wcccdesign.com/plas/chapter2/HandsOnProject2-3/index.html";
+var proj24 = document.getElementById("2-4").href = "http://wcccdesign.com/plas/chapter2/HandsOnProject2-4/index.html";
+var tut2 = document.getElementById("tut2").href = "http://wcccdesign.com/plas/chapter2/tutorial/index.html";
+
+//Chapter 3
+var turbine = document.getElementById("turbine").href = "http://wcccdesign.com/plas/chapter3/chapter/calendar.html";
+var proj31 = document.getElementById("3-1").href = "http://wcccdesign.com/plas/chapter3/HandsOnProject3-1/index.html";
+var proj34 = document.getElementById("3-4").href = "http://wcccdesign.com/plas/chapter3/HandsOnProject3-4/index.html";
+
+//Chapter 4
+var tractor = document.getElementById("tractor").href = "http://wcccdesign.com/plas/Chapter4/chapter/tractor.html";
+var proj45 = document.getElementById("4-5").href = "http://wcccdesign.com/plas/Chapter4/HandsOnProject4-5/index.html";
+var browser = document.getElementById("crashBrowser").href = "http://wcccdesign.com/plas/Chapter4/crashMyBrowser/index.html";
+
+//Chapter 5
+var photoSlide = document.getElementById("photoSlide").href = "http://wcccdesign.com/plas/Chapter5/chapter/photos.html";
+var proj52 = document.getElementById("5-2").href = "http://wcccdesign.com/plas/Chapter5/HandsOnProject5-2/index.html";
+var proj53 = document.getElementById("5-3").href = "http://wcccdesign.com/plas/Chapter5/HandsOnProject5-3/index.html";
+var proj54 = document.getElementById("5-4").href = "http://wcccdesign.com/plas/Chapter5/HandsOnProject5-4/index.html";
+
+//Chapter 6
+var snoot = document.getElementById("snoot").href = "http://wcccdesign.com/plas/Chapter6/Chapter/snoot.html";
+var proj61 = document.getElementById("6-1").href = "http://wcccdesign.com/plas/Chapter6/HandsOnProject6-1/index.html";
+var proj64 = document.getElementById("6-4").href = "http://wcccdesign.com/plas/Chapter6/HandsOnProject6-4/index.html";
+
+//footer mailto tag
+var mail = document.getElementById("mail").href = "mailto:plass@my.wccc.edu"
