1ce47b7ac4efc5e6a7bfee2e908b15e082e8c8a9
stevenPlas <steven.h.plas@gmail.com>
2017-03-24 10:42:59 -0400

    Adding all files for chapter 8

---
+++ Chapter8/HandsOnProject8-1/script.js
@@ -0,0 +1,60 @@
+/*
+JavaScript 6th Edition
+Chapter 8
+Hands-on Project 8-1
+
+Author: Steven Plas
+Date:   3/24/17
+
+Filename: script.js
+*/
+"use strict";
+
+//global variables
+var newAccountArray = [];
+
+function createID() {
+    var fname = document.getElementById("fnameinput");
+    var lname = document.getElementById("lnameinput");
+    var zip = document.getElementById("zipinput");
+    var account = document.getElementById("accountidbox");
+    var fields = document.getElementsByTagName("input");
+    var acctid;
+    var firstInit;
+    var lastInit;
+
+    if (fname.value !== "" && lname.value !== "" && zip.value !== "") {
+        firstInit = fname.value.charAt(0).toUpperCase();
+        lastInit = lname.value.charAt(0).toUpperCase();
+        acctid = firstInit + lastInit + zip.value;
+        account.value = acctid;
+        newAccountArray = [];
+        for (var i = 0; i < fields.length; i++) {
+            newAccountArray.push(fields[i].value);
+        }
+    }
+
+}
+
+//Create event listeners
+function createEventListeners() {
+    var fname = document.getElementById("fnameinput");
+    var lname = document.getElementById("lnameinput");
+    var zip = document.getElementById("zipinput");
+
+    if (fname.addEventListener) {
+        fname.addEventListener("change", createID, false);
+        lname.addEventListener("change", createID, false);
+        zip.addEventListener("change", createID, false);
+    } else if (fname.attachEvent) {
+        fname.attachEvent("onchange", createID);
+        lname.attachEvent("onchange", createID);
+        zip.attachEvent("onchange", createID);
+    }
+}
+
+if (window.addEventListener) {
+    window.addEventListener("load", createEventListeners, false);
+} else if (window.attachEvent) {
+    window.attachEvent("onload", createEventListeners);
+}
