c0c9b23181a43a139785eedf85d7fb10c787a832
Steven Plas <steven.h.plas@gmail.com>
2017-04-22 16:41:51 -0400

    Adding updates for the project.

--- project/script.js
+++ project/script.js
@@ -2,6 +2,10 @@
 var templateSource = document.getElementById('results-template').innerHTML;
 var resultsPlaceholder = document.getElementById('results');
 var template = Handlebars.compile(templateSource);
+var lastFmUrl1 =  'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=';
+var bandName = document.getElementById('query').value;
+var lastFmUrl2 = '&api_key=3727d6680ea7c0950e38739913861eb7&format=json';
+var fullUrl = lastFmUrl1 + bandName + lastFmUrl2;
 
 
 var searchAlbums = function(query) {
@@ -17,26 +21,32 @@ var searchAlbums = function(query) {
     });
 };
 
-var searchLastFM = function(query) {
+
+
+var searchLastFM = function(fullUrl) {
   $.ajax({
-    url: ' http://ws.audioscrobbler.com/2.0/',
+    url: fullUrl,
     data:{
       q:query,
-      type: 'Artist'
+      type: 'artist'
     },
-    success: function (data) {
-      variable = data;
-    }
-  });
-  return variable;
+    success: function(result){
+        $("#information").html(result);
+    }});
 }
 
+/*
+$("button").click(function(){
+   $.ajax({url: "demo_test.txt", success: function(result){
+       $("#div1").html(result);
+   }});
+});
+*/
 
 results.addEventListener('click', function(e) {
     e.preventDefault();
-    var object = document.getElementById('query').value;
 
-    document.getElementById('information').innerHTML = searchLastFM(object.artist.getinfo);
+    document.getElementById('information').innerHTML = searchLastFM(fullUrl);
 }, false);
 
 document.getElementById('search-form').addEventListener('submit', function(e) {
